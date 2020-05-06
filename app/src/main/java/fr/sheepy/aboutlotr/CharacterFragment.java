8e867a8e86998f3354bd2bf4a22d644a823027a1
Sheepy <calu1999@gmail.com>
2020-05-06 16:57:52 +0200

    Finalisation du fragment d'affichage des details d'un character

--- app/src/main/java/fr/sheepy/aboutlotr/CharacterFragment.java
+++ app/src/main/java/fr/sheepy/aboutlotr/CharacterFragment.java
@@ -15,7 +15,7 @@ import androidx.fragment.app.Fragment;
 
 public class CharacterFragment extends Fragment {
 
-    private TextView name, race, birth, death;
+    private TextView name, race, height, birth, death, spouse, realm, URL;
     private ImageView icon, gender;
     private Character character;
     private Gson gson;
@@ -25,10 +25,15 @@ public class CharacterFragment extends Fragment {
                              Bundle savedInstanceState) {
         View characterView = inflater.inflate(R.layout.character_fragment, container, false);
         icon = characterView.findViewById(R.id.charaIcon);
+        gender = characterView.findViewById(R.id.charaGender);
         name = characterView.findViewById(R.id.charaName);
         race = characterView.findViewById(R.id.charaRace);
+        height = characterView.findViewById(R.id.charaHeight);
         birth = characterView.findViewById(R.id.charaBirth);
         death = characterView.findViewById(R.id.charaDeath);
+        spouse = characterView.findViewById(R.id.charaSpouse);
+        realm = characterView.findViewById(R.id.charaRealm);
+        URL = characterView.findViewById(R.id.charaURL);
         return characterView;
     }
 
@@ -40,16 +45,75 @@ public class CharacterFragment extends Fragment {
                 .setLenient()
                 .create();
 
-        assert getArguments() != null;
         String temp = CharacterFragmentArgs.fromBundle(getArguments()).getCharacterInfo();
         if (temp != null) {
             character = gson.fromJson(temp, Character.class);
+            // icon du character
             icon.setImageResource(character.getRaceResource());
-            name.setText(character.getName());
-            race.setText(character.getRace());
-            birth.setText(character.getBirth());
-            death.setText(character.getDeath());
+
+            // nom du character
+            if (character.getName() != null && !character.getName().equals("")) {
+                name.setText(character.getName());
+            }
+
+            // genre du character
+            if (character.getGender().equals("Male")) {
+                gender.setImageResource(R.drawable.boy);
+            } else if (character.getGender().equals("Female")) {
+                gender.setImageResource(R.drawable.boy);
+            } else {
+                gender.setVisibility(View.GONE);
+            }
+
+            // race du character
+            if (character.getRace() != null) {
+                race.setText(character.getRace());
+            } else {
+                race.setText("Unknown Race");
+            }
+
+            // height du character
+            if (character.getHeight() != null && !character.getHeight().equals("")) {
+                height.setText("Height: " + character.getHeight());
+            } else {
+                height.setVisibility(View.GONE);
+            }
+
+            // birth du character
+            if (character.getBirth() != null && !character.getBirth().equals("")) {
+                birth.setText("Birth: " + character.getBirth());
+            } else {
+                birth.setVisibility(View.GONE);
+            }
+
+            // death du character
+            if (character.getDeath() != null && !character.getDeath().equals("")) {
+                death.setText("Death: " + character.getDeath());
+            } else {
+                death.setVisibility(View.GONE);
+            }
+
+            // Spouse du character
+            if (character.getSpouse() != null && !character.getSpouse().equals("")) {
+                spouse.setText("Spouse: " + character.getSpouse());
+            } else {
+                spouse.setVisibility(View.GONE);
+            }
+
+            // Realm du character
+            if (character.getRealm() != null && !character.getRealm().equals("")) {
+                realm.setText("Realm: " + character.getRealm());
+            } else {
+                realm.setVisibility(View.GONE);
+            }
+
+            // URL du character
+            if (character.getWikiUrl() != null && !character.getWikiUrl().equals("")) {
+                URL.setText("Wiki link:\n" + character.getWikiUrl());
+            } else {
+                URL.setText("Unknown Wiki Url");
+            }
+
         }
     }
-
 }
